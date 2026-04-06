5ab1d61f83b3684040d2bfc24715502849bd0a93
KeanBM08 <keanmacalisang2008@gmail.com>
2026-04-05 20:42:24 -0700

    Fix slideshow

--- javascript.js
+++ javascript.js
@@ -1,10 +1,14 @@
-const slides = document.querySelectorAll('.slideshow .slide');
-let currentSlide = 0;
+window.addEventListener("DOMContentLoaded", () => {
+  const slides = document.querySelectorAll('.slideshow .slide');
+  let currentSlide = 0;
 
-if (slides.length > 0) {
-  setInterval(() => {
-    slides[currentSlide].classList.remove('active');
-    currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].classList.add('active');
-  }, 3500);
-}
+  if (slides.length > 1) {
+    setInterval(() => {
+      slides[currentSlide].classList.remove('active');
+
+      currentSlide = (currentSlide + 1) % slides.length;
+
+      slides[currentSlide].classList.add('active');
+    }, 3000);
+  }
+});
